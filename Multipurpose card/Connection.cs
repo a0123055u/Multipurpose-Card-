49db6761ee7399d7b4aa5ec38b008b20035094f7
a0123055u <festecole06>
2015-07-26 10:39:55 +0800

    Entiry model chaged based on updated version

--- Multipurpose card/Connection.cs
+++ Multipurpose card/Connection.cs
@@ -8,16 +8,17 @@ namespace Multipurpose_card
 {
     class Connection
     {
-        String result = "Not found";
+        String result = "";
         String temp1;
         public String Authentication(String user, String password)
         {
-            usersofsinglecard urssinglecrd = new usersofsinglecard();
+           //SingleCardApplicationEntities urssinglecrd = new SingleCardApplicationEntities();
+            
             try
             {
-                using (AuthenticationEntities1 authEntity = new AuthenticationEntities1())
+                using (SingleCardApplicationEntities likedobject = new SingleCardApplicationEntities())
                 {
-                    var checkpass = (from y in authEntity.usersofsinglecards
+                    var checkpass = (from y in likedobject.bankusers
                                      where y.userid == user
                                      select y.passwords);
                     foreach (var temp in checkpass)
@@ -28,16 +29,47 @@ namespace Multipurpose_card
                     }
                     if (temp1.Equals(password))
                     {
-                        var check = (from x in authEntity.usersofsinglecards
+                        var check = (from x in likedobject.bankusers
                                      where x.userid == user
-                                     select new { x.acc1name, x.acc2name,x.username});
-                        foreach(var s in check)
+                                     select new { x.username });
+                        String usernameforreturn;
+                        if(check!=null)
+                             usernameforreturn = check.FirstOrDefault().ToString();
+                        var accuts = (from acc in likedobject.useraccounts
+                                      where acc.userid == user
+                                      select  ( acc.bankid ));
+                        List<String> list = accuts.ToList<string>();
+                        //accuts.Count();
+                        String[] bankid = new String[accuts.Count()];
+                        int count=0;
+                        if(accuts!=null)
                         {
-                            result = s.acc1name+","+s.acc2name+","+s.username;
-                            
-                        }
-                        return result;
+                            foreach(var ids in accuts)
+                            {
+                                bankid[count] = ids.ToString();
+                                count++;
 
+                            }
+                        }
+                        int controller = bankid.Count();
+                        String[] banknames= new String[10];
+                        for (int cc = 0; cc < controller; cc++)
+                        {
+                            //store current value and finds it 
+                            String tt = bankid[cc];
+                            
+
+                            var displaynames = (from bak in likedobject.banks
+                                                where bak.bankid == tt
+                                                select bak.bankname);
+                            banknames[cc] = displaynames.FirstOrDefault().ToString();
+                        }
+                       
+                            result =String.Join(",",banknames);
+                        
+
+                        Console.WriteLine(result);
+                        return result;
                     }                       
                        
                     else
